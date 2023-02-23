7c1b93312ee6a3618c46b8e8a8b5d088f7992a75
Sekou Samassa <sksamassa@gmail.com>
2023-02-23 17:27:25 +0500

    New release with binding methods

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,23 +3,40 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.diceroller.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.rollButton.setOnClickListener {
             rollDice()
         }
+
     }
-
     private fun rollDice() {
-       val resultText: TextView = findViewById(R.id.result_text)
-        val randNum = (Math.floor(Math.random() * 100) + 1).toInt()
 
-        resultText.text = randNum.toString()
+        binding.apply {
+            invalidateAll()
+            val randNum = (1..6).random()
+            val drawableResource = when(randNum){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            diceImage.setImageResource(drawableResource)
+        }
     }
 }
+
